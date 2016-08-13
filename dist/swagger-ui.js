1e02b7501dd34defd14c03a24170558c00403ad2
Christian <christiancbergstrom@gmail.com>
2016-08-13 11:56:15 -0700

    update tabs & spacing for param_complex_query

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -789,7 +789,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</label>\n    <form class=\"query-choices\"></form>\n		<p class=\"c-help-text\">";
+    + "</label>\n    <form class=\"query-choices\"></form>\n    <p class=\"c-help-text\">";
   if (stack1 = helpers.paramType) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.paramType; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   if(stack1 || stack1 === 0) { buffer += stack1; }
